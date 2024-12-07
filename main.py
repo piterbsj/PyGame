0fb0928d6f774803b69733d202e2742691d5f488
piterbsj <aleksandracarova057@gmail.com>
2024-12-07 17:05:20 +0300

    начало

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('kfkf')
+import pygame
+from pygame import*
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = w, h = 1000, 600
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((216, 191, 216))
+        pygame.display.flip()
