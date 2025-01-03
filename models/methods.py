a9765eecb517580e82cd5212c47aa0b639d34d40
Valera-nchk <vlinter@icloud.com>
2025-01-03 19:24:07 +0300

    добавление методов, спрайты

---
+++ models/methods.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+import sys
+
+
+def load_image(name):
+    if not os.path.isfile(name):
+        print(f"Файл с изображением '{name}' не найден")
+        sys.exit()
+    image = pygame.image.load(name)
+    return image
