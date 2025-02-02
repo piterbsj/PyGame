24919cb7fc492acfa55069958b2559dd577126d8
Valera-nchk <vlinter@icloud.com>
2025-02-02 18:40:28 +0300

    мир кубиков

--- models/interface.py
+++ models/interface.py
@@ -19,19 +19,29 @@ class Obstacles:                #препятствия
     def newrect(self):
         for obst in self.coord:
             box_hitbox = pygame.rect.Rect(obst[0], obst[1], obst[2], obst[3])
-            box_color = pygame.color.Color((253, 233, 16))
-            self.newcoord.append([box_hitbox, box_color, box_hitbox.x])
+
+            self.x, self.y = 0, 0
+            self.all_sprites_obst = pygame.sprite.Group()
+            sprite_obst = pygame.sprite.Sprite()
+            sprite_obst.image = pygame.image.load('image/obd.jpg')
+            sprite_obst.image = pygame.transform.scale(sprite_obst.image, (50, 50))
+            self.mask = pygame.mask.from_surface(sprite_obst.image)
+            sprite_obst.rect = box_hitbox
+            self.all_sprites_obst = pygame.sprite.GroupSingle()
+            self.all_sprites_obst.add(sprite_obst)
+
+            self.newcoord.append([self.all_sprites_obst, box_hitbox, box_hitbox.x])
 
     def draw(self, screen: pygame.Surface):
         for obst in self.newcoord:
-            pygame.draw.rect(screen, obst[1], obst[0], width=0)
+            obst[0].draw(screen)
 
     def moveall(self, keys):
         if keys[pygame.K_RIGHT]:
             for i in self.newcoord:
                 i[2] -= (Obstacles.SPEED * 150) / 1000
-                i[0].x = i[2]
+                i[1].x = i[2]
         if keys[pygame.K_LEFT]:
             for i in self.newcoord:
                 i[2] += (Obstacles.SPEED * 150) / 1000
-                i[0].x = i[2]
+                i[1].x = i[2]
